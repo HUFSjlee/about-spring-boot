7175e0d6bf4422b3150ad960c7f5d7f0dd388840
deepsj1012 <deepsj1012@naver.com>
2026-01-30 16:51:59 +0900

    자유롭게 시작!

---
+++ src/main/java/com/example/aboutspringboot/AboutspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aboutspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aboutspringboot/AboutspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aboutspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
